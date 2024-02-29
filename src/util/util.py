86719979e1fd66d972696a4c6b1d51dc2c939508
Kristján Jónasson <jonasson@hi.is>
2024-02-29 11:55:09 +0000

    fixed errors

--- src/util/util.py
+++ src/util/util.py
@@ -8,6 +8,15 @@ def install_import(package_name):
   pkg = importlib.import_module(package_name)
   return pkg
 
+def install_if_missing(package_name):
+  import os, pkg_resources
+  try:
+    pkg_resources.get_distribution(package_name)
+    return
+  except:
+    os.system(f"pip -q install {package_name}")
+    return False
+
 def addpath_if_missing(p):
   import sys
   if p not in sys.path:
